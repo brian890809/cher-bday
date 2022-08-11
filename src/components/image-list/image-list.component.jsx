0682377a775e4e13c170d9bd3c43d49df1d31073
brian890809 <brian08090809@gmail.com>
2022-08-11 01:41:27 -0700

    added image list

---
+++ src/components/image-list/image-list.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import Image from './../pic-frame/pic-frame.component'
+
+export default class WovenImageList extends React.Component {
+    render() {
+        var imageList = this.props.imageList;
+        return (
+            <ImageList variant="woven" cols={this.props.cols} gap={this.props.gap}>
+                {imageList.map((imageItem) => (
+                    <ImageListItem>
+                        <Image rounded imgSrc={imageItem.imgSrc}/>
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        )
+    }
+}
